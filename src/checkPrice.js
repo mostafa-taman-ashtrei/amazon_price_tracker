733cafb6c3edc91d3f7c6858fc5cb285b464c24d
NEO <neo.one.mail@gmail.com>
2021-01-14 04:12:38 +0200

    set up telegram bot && create checkPrice command

--- src/checkPrice.js
+++ src/checkPrice.js
@@ -1,6 +1,16 @@
-const checkPrice = async (page) => {
+const pupppeteer = require('puppeteer');
+
+const checkPrice = async () => {
+    const ProductUrl = 'https://www.amazon.com/Potensic-Quadcopter-Portable-Beginner-Expert-Upgrade/dp/B08JQ5X7DD/ref=sr_1_1_sspa?crid=1AE1AWCT46QE2&dchild=1&keywords=drones+with+camera+for+adults&qid=1610581680&sprefix=drones%2Caps%2C302&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMVJLSU1JWjE3NjBZJmVuY3J5cHRlZElkPUEwNTQ4MzA5MjQ0UEVDRTlJUTEyNiZlbmNyeXB0ZWRBZElkPUEwMjU4OTY2Nk9MSVNCVUlYQ0xZJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==';
+
     try {
+        const browser = await pupppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(ProductUrl, { waitUntil: 'load', timeout: 0 });
         await page.reload();
+
+        page.setViewport({ width: 900, height: 1500 });
+        await page.screenshot({ path: `src/screenshot${Date.now()}.png` });
 
         const price = await page.evaluate(() => document.getElementById('priceblock_ourprice').innerHTML);
         const inStockSpan = await page.evaluate(() => document.getElementById('availability').querySelector('span').innerHTML);
@@ -10,8 +20,16 @@ const checkPrice = async (page) => {
 
         console.log(currentPrice);
         console.log(inStock);
+
+        await browser.close();
+
+        return {
+            currentPrice,
+            inStock,
+        };
     } catch (e) {
         console.log(e);
+        throw new Error('web scraping failed due to some error');
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-const pupppeteer = require('puppeteer');
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
 
 const checkPrice = require('./checkPrice');
 
 (async () => {
-    const ProductUrl = 'https://www.amazon.com/Potensic-Quadcopter-Portable-Beginner-Expert-Upgrade/dp/B08JQ5X7DD/ref=sr_1_1_sspa?crid=1AE1AWCT46QE2&dchild=1&keywords=drones+with+camera+for+adults&qid=1610581680&sprefix=drones%2Caps%2C302&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMVJLSU1JWjE3NjBZJmVuY3J5cHRlZElkPUEwNTQ4MzA5MjQ0UEVDRTlJUTEyNiZlbmNyeXB0ZWRBZElkPUEwMjU4OTY2Nk9MSVNCVUlYQ0xZJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==';
-    const browser = await pupppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(ProductUrl, { waitUntil: 'load', timeout: 0 });
-    await checkPrice(page);
-    await browser.close();
+    const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+    console.log('Seatch for Neobot and use /start command to get started');
+
+    bot.use(async (_, next) => {
+        const start = new Date();
+        await next();
+        const ms = new Date() - start;
+        console.log('Response time: %sms', ms);
+    });
+
+    bot.start((ctx) => { ctx.reply(`Welcome to neobot, ${ctx.message.from.first_name}`); });
+
+    bot.command('checkPrice', async (ctx) => {
+        ctx.telegram.sendMessage(ctx.chat.id, 'checking ...');
+        const { currentPrice, inStock } = await checkPrice();
+        ctx.telegram.sendMessage(
+            ctx.message.chat.id,
+            `current price is ${currentPrice} and the item is ${inStock}`,
+        );
+    });
+
+    bot.launch();
 })();
